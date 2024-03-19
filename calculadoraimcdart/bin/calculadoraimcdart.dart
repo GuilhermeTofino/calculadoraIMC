d1bd742744ab83a04f7336709fd5ff42507746af
Guilherme <gpulcino4@gmail.com>
2024-03-19 11:28:17 -0300

    Commit inicial

---
+++ calculadoraimcdart/bin/calculadoraimcdart.dart
@@ -0,0 +1,5 @@
+import 'package:calculadoraimcdart/calculadoraimcdart.dart' as calculadoraimcdart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculadoraimcdart.calculate()}!');
+}
---
+++ calculadoraimcdart/test/calculadoraimcdart_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculadoraimcdart/calculadoraimcdart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
